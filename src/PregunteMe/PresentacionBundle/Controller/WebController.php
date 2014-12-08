e117d37d565d89c3b67b39cf530bb8dab852efce
Jorge Gonzalez <jag2kn@gmail.com>
2014-12-08 16:08:34 -0500

    Se agregan funciones necesarias para el registro de usuarios y login

--- src/PregunteMe/PresentacionBundle/Controller/WebController.php
+++ src/PregunteMe/PresentacionBundle/Controller/WebController.php
@@ -2,8 +2,13 @@
 
 namespace PregunteMe\PresentacionBundle\Controller;
 
+use Symfony\Component\Security\Core\SecurityContext;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use PregunteMe\AdministracionBundle\Form\UsuarioType;
+
+use PregunteMe\AdministracionBundle\Entity\Usuario;
 use PregunteMe\AdministracionBundle\Entity\Indicador;
 use PregunteMe\AdministracionBundle\Entity\CasoEstudio;
 
@@ -101,5 +106,69 @@ class WebController extends Controller
 		return $this->redirect($this->generateUrl('index'));
 		
 	}
+	
+	
+	
+	///////////////////// Manejo de usuarios
+	
+	public function registroAction()
+    {
+    	$request = $this->get('request');
+		$entity = new Usuario();
+        $form = $this->createForm(new UsuarioType(), $entity, array(
+            'action' => $this->generateUrl('registro'),
+            'method' => 'POST',
+        ));
 
+        $form->add('submit', 'submit', array('label' => 'Registrarse'));
+        
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            
+            $factory = $this->get('security.encoder_factory');
+            $encoder = $factory->getEncoder($entity);
+			$password = $encoder->encodePassword($entity->getPassword(), $entity->getSalt());
+			$entity->setPass($password);
+
+            
+            $em->persist($entity);
+            $em->flush();
+            
+            //ToDo: Colocar en la bolsa el mensaje sobre el registro
+
+            return $this->redirect($this->generateUrl('index'));
+        }
+
+        return $this->render('PregunteMePresentacionBundle:Web:registro.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+
+    }
+
+    public function loginAction()
+    {
+		$request = $this->getRequest();
+		$session = $request->getSession();
+		// get the login error if there is one
+		if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+			$error = $request->attributes->get(
+				SecurityContext::AUTHENTICATION_ERROR
+			);
+		} else {
+			$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+			$session->remove(SecurityContext::AUTHENTICATION_ERROR);
+		}
+
+		return $this->render(
+				'PregunteMePresentacionBundle:Web:login.html.twig',
+				array(
+					// last username entered by the user
+					'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+					'error'=> $error,
+				)
+			);
+	}
 }
